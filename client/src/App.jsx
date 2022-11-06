61f6b71672d7357457ba675bf1dbee855fa135c9
zeshancph <cph-za54@cphbusiness.dk>
2022-11-06 20:27:48 +0100

    mainPage is added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import FrontPage from "./components/FrontPage/FrontPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FrontPage />
       <Footer />
     </div>
   );
