c78057327a239d059db28cccf36b5c7ef2bd8a58
lelekaspb <bov-1986@yandex.ru>
2022-12-31 14:34:09 +0100

    added postmodule to testmodule

--- server/src/test.module.ts
+++ server/src/test.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
-import { AppModule } from './app.module';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { OrchestraModule } from './orchestra/orchestra.module';
+import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
-import { UserService } from './user/user.service';
 
 @Module({
   imports: [
@@ -14,6 +13,7 @@ import { UserService } from './user/user.service';
     UserModule,
     OrchestraModule,
     AuthModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService, OrchestraModule, AuthModule],
